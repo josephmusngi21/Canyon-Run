e37aba9355faf8e0f8a5fd59d7713b92c90b4836
Josephmusngi21 <josephmusngi7@gmail.com>
2025-04-01 00:32:41 -0700

    p

--- client/App.js
+++ client/App.js
@@ -11,7 +11,6 @@ export default function App() {
   const handleLogin = () => {
     setIsAuthenticated(true);
   };
-
   return (
     <View style={styles.container}>
       {/* {!isAuthenticated ? (
