a2953ec301a679b79e00e36c831d1c4537f5c81d
Josephmusngi21 <josephmusngi7@gmail.com>
2025-04-28 20:01:31 -0700

    commented out imports to debug problem

--- client/App.js
+++ client/App.js
@@ -1,25 +1,21 @@
 import { StyleSheet, View, Text } from 'react-native';
 import React, { useState } from 'react';
-//Maps
-import Maps from './components/pages/Maps/Maps';
-//Altitude
-import Altitude from './components/pages/Maps/Altitude';
-//AddRun
-import AddRun from './components/pages/Add/AddRun';
-import { Button } from 'react-native-web';
+// //Maps
+// import Maps from './components/pages/Maps/Maps';
+// //Altitude
+// import Altitude from './components/pages/Maps/Altitude';
+// //AddRun
+// import AddRun from './components/pages/Add/AddRun';
+// import { Button } from 'react-native-web';
 
 export default function App() {
-  const [display, setDisplay] = useState(false);
 
   return (
     <View style={styles.container}>
-    {display ? <View><Maps/><Altitude/>
-    <Button title="Hide Maps" onPress={() => setDisplay(false)} />
-    </View> : 
-    <Button title="Show Maps" onPress={() => setDisplay(true)} />}
       {/* <Maps />
       <Altitude /> */}
-      <AddRun />
+      {/* <AddRun /> */}
+      <Text>Hello World</Text>
     </View>
   );
 }
@@ -33,3 +29,4 @@ const styles = StyleSheet.create({
     overflow: 'auto',
   },
 });
+
