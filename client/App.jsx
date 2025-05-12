8d81777166a5017de990ea6848da6e81202b9bda
josephmusngi21 <josephmusngi7@gmail.com>
2025-05-12 00:53:55 -0700

    removed react native web import and moved button to just react native, also commented out maps and altitude to figure out issue

---
+++ client/App.jsx
@@ -0,0 +1,34 @@
+import { StyleSheet, View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+//Maps
+import Maps from './components/pages/Maps/Maps';
+//Altitude
+import Altitude from './components/pages/Maps/Altitude';
+//AddRun
+import AddRun from './components/pages/Add/AddRun';
+
+export default function App() {
+  const [display, setDisplay] = useState(false);
+
+  return (
+    <View style={styles.container}>
+    {/* {display ? <View><Maps/><Altitude/>
+    <Button title="Hide Maps" onPress={() => setDisplay(false)} />
+    </View> : 
+    <Button title="Show Maps" onPress={() => setDisplay(true)} />} */}
+      {/* <Maps /> */}
+      {/* <Altitude /> */}
+      <AddRun />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'auto',
+  },
+});
