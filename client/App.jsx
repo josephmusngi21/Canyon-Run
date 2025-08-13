bcac77bceab052e8e118d900b0a70cb43024471a
Josephmusngi21 <josephmusngi7@gmail.com>
2025-08-13 01:23:58 -0700

    added file manager and conditional for once file was uploaded

--- client/App.jsx
+++ client/App.jsx
@@ -1,71 +1,37 @@
-import { StyleSheet, View, Text, Button, ScrollView } from "react-native";
 import React, { useState } from "react";
-// Components
-import Maps from "./components/pages/Maps/Maps";
-import Altitude from "./components/pages/Maps/Altitude";
-import AddRun from "./components/pages/Add/AddRun";
+import { StyleSheet, View } from "react-native";
+import Track from './components/pages/Tracker/Track';
+import FileManager from './components/FileManager';
 
 export default function App() {
-  const [display, setDisplay] = useState(false);
+  const [fileReady, setFileReady] = useState(false);
+  const [savedRuns, setSavedRuns] = useState({});
 
-  const showingMaps = () => {
-
-
-    return (
-      <View style={styles.mapContainer}>
-        <Maps />
-        <Altitude />
-      </View>
-    )
-  }
+  const handleFileReady = (ready, runsData = {}) => {
+    setFileReady(ready);
+    setSavedRuns(runsData);
+  };
 
   return (
     <View style={styles.container}>
-      {/* <ScrollView styles={styles.scrollContainer}>
-        {display ? (
-          <View>
-            <Button 
-            title="Hide"
-            onPress={() => {
-              setDisplay(!display);
-            }}
-            />
-          </View>
-        ) : (
-          <Button 
-          title="Show"
-          onPress={() => {
-            setDisplay(!display);
-          }}
-          />
-        )}
-      </ScrollView> */}
-      {/* <AddRun /> */}
-      {/* <Maps /> */}
-      {/* <Text>Hello</Text> */}
-      <Text>Hello</Text>
+      {!fileReady ? (
+        <FileManager onFileReady={handleFileReady} />
+      ) : (
+        <Track 
+          initialSavedRuns={savedRuns} 
+          onFileManagerRequest={() => setFileReady(false)} 
+        />
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  scrollContainer: {
-    flexGrow: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingBottom: 50, // Extra padding for better scrolling
-  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    padding: 20,
-  },
-  mapContainer: {
-    width: "90%",
-    minHeight: 500, // Allows scrolling
-    backgroundColor: "#f0f0f0",
     padding: 20,
   },
 });
